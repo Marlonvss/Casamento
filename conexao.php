5f6f98549f3770c08eca028e283c4595ab622340
Marlon Vitor <Marlon Vitor>
2016-02-07 17:37:29 -0200

    Refactor para automatizar conexao com bd

--- conexao.php
+++ conexao.php
@@ -1,20 +1,26 @@
 <?php
+
 error_reporting(E_ERROR);
 
 function Conecta() {
 
-//    Configuração Local...
-//    $Host = 'localhost';
-//    $User = 'root';
-//    $Pass = '';
-//    $BD = 'casamento';
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
 
-//    Configuração HOSTINGER...
-    $Host = 'mysql.hostinger.com.br';
-    $User = 'u812671016_casam';
-    $Pass = 'casamento';
-    $BD = 'u812671016_casam';
+        // Configuração Local...
+        $Host = 'localhost';
+        $User = 'root';
+        $Pass = '';
+        $BD = 'casamento';
+        
+    } else {
 
+        // Configuração HOSTINGER...
+        $Host = 'mysql.hostinger.com.br';
+        $User = 'u812671016_casam';
+        $Pass = 'casamento';
+        $BD = 'u812671016_casam';
+        
+    }
 
     $connect = mysql_connect($Host, $User, $Pass) or die(mysql_error());
     $db = mysql_select_db($BD) or die(mysql_error());
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-echo $_SERVER['HTTP_HOST']; 
 error_reporting(E_ERROR);
 
 session_start();
