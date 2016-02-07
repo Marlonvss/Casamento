ecd37cbd6975f2ed52720cef7a938e67143478d8
Marlon Vitor <Marlon Vitor>
2016-02-07 17:40:30 -0200

    Correcao da frase inicial

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <h1>Rafaelle e Marlon Vitor</h1>
         <hr>
         <p>Muito obrigado por fazer parte da nossa história e compartilhar conosco momentos inesquecíveis.
-            </br>Não deixe de confirmar sua presença para que você possa viver mais um momento na nossa história, momento esse que vocês nos verá solteiros pela última vez, casados para sempre e felizes como sempre e pra sempre</p>
+            </br>Não deixe de confirmar sua presença para que você possa viver mais um momento na nossa história, momento esse que vocês nos verá solteiros pela última vez, casados para sempre e felizes como sempre e pra sempre.</p>
         <a href="#convidados" class="btn btn-primary btn-xl page-scroll">Confirme sua presença</a>
     </div>
 </div>
