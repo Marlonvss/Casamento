558d7d7249341902df368503bf5cb5780ca7a5d5
Marlon Vitor <Marlon Vitor>
2016-02-08 21:31:01 -0200

    teste

--- conexao.php
+++ conexao.php
@@ -4,23 +4,23 @@ error_reporting(E_ERROR);
 
 function Conecta() {
 
-//    if ($_SERVER['HTTP_HOST'] == 'localhost') {
-//
-//        // Configuração Local...
-//        $Host = 'localhost';
-//        $User = 'root';
-//        $Pass = '';
-//        $BD = 'casamento';
-//        
-//    } else {
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
+
+        // Configuração Local...
+        $Host = 'localhost';
+        $User = 'root';
+        $Pass = '';
+        $BD = 'casamento';
+        
+    } else {
 
         // Configuração HOSTINGER...
-        $Host = 'mysql.hostinger.com.br';
+        $Host = '127.0.0.1';
         $User = 'u812671016_casam';
         $Pass = 'casamento';
         $BD = 'u812671016_casam';
         
-//    }
+    }
 
     $connect = mysql_connect($Host, $User, $Pass) or die(mysql_error());
     $db = mysql_select_db($BD) or die(mysql_error());
