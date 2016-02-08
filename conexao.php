103e2dbba1a767b1c34ef165c86b698fb5bb8083
Marlon Vitor <Marlon Vitor>
2016-02-08 21:04:59 -0200

    tentando acertar o erro

--- conexao.php
+++ conexao.php
@@ -4,15 +4,15 @@ error_reporting(E_ERROR);
 
 function Conecta() {
 
-    if ($_SERVER['HTTP_HOST'] == 'localhost') {
-
-        // Configuração Local...
-        $Host = 'localhost';
-        $User = 'root';
-        $Pass = '';
-        $BD = 'casamento';
-        
-    } else {
+//    if ($_SERVER['HTTP_HOST'] == 'localhost') {
+//
+//        // Configuração Local...
+//        $Host = 'localhost';
+//        $User = 'root';
+//        $Pass = '';
+//        $BD = 'casamento';
+//        
+//    } else {
 
         // Configuração HOSTINGER...
         $Host = 'mysql.hostinger.com.br';
@@ -20,7 +20,7 @@ function Conecta() {
         $Pass = 'casamento';
         $BD = 'u812671016_casam';
         
-    }
+//    }
 
     $connect = mysql_connect($Host, $User, $Pass) or die(mysql_error());
     $db = mysql_select_db($BD) or die(mysql_error());
