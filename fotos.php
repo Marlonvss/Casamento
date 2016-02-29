59f348a6b3602dc5284c0eed205aae6a86323ecc
Marlon Vitor <Marlon Vitor>
2016-02-29 00:11:42 -0300

    FOtos

--- fotos.php
+++ fotos.php
@@ -1,192 +1,27 @@
 <script>
-    
-$("#show").on("click", function() {
-    alert('Ha');
-   $(this).modal();
-});
+
+    $("#show").on("click", function () {
+        alert('Ha');
+        $(this).modal();
+    });
 </script>
 
 <div class="container-fluid">
     <div class="row no-gutter">
-        <div class="col-lg-4 col-sm-6">
-            <a id="show" class="fotos-box">
-                <img src="img/fotos/1.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category MY NAME
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/2.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/3.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/4.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/5.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/6.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/1.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/2.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/3.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/4.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/5.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <a href="#" class="fotos-box">
-                <img src="img/fotos/6.jpg" class="img-responsive" alt="">
-                <div class="fotos-box-caption">
-                    <div class="fotos-box-caption-content">
-                        <div class="project-category text-faded">
-                            Category
-                        </div>
-                        <div class="project-name">
-                            Project Name
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/0.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/1.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/2.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/3.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/4.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/5.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/6.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/7.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/8.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/9.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/10.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/11.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/12.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/13.jpg" class="img-responsive" alt=""></div>
+        <div class="col-lg-4 col-sm-6"><img src="img/fotos/14.jpg" class="img-responsive" alt=""></div>
     </div>
 </div>
